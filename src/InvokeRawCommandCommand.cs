3f0f747165a6f51f1a69262660fad513c078e429
teramako <teramako@gmail.com>
2026-07-07 00:28:26 +0900

    👍 Add ScriptBlock validation: support only single external command
    - Introduce SniffScriptBlock to check for:
      - no Begin/Process blocks
      - EndBlock with exactly one statement
      - PipelineAst with a single CommandAst
    - Preserve existing raw { cmd } behavior
    - Throw NotSupported for complex ScriptBlocks (pipelines, multiple statements)

--- src/InvokeRawCommandCommand.cs
+++ src/InvokeRawCommandCommand.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Management.Automation;
 using System.Management.Automation.Language;
 using System.Text;
@@ -76,7 +77,20 @@ public class InvokeRawCommandCommand : RawCommandBase
         {
             if (Script is not null)
             {
-                (appInfo, Arguments) = GetCommandAndArguments(Script);
+                if (SniffScriptBlock(Script, out var commandAst))
+                {
+                    appInfo = GetAppInfo(commandAst.GetCommandName());
+                    Arguments = GetArguments(commandAst).ToArray();
+                }
+                else
+                {
+                    ThrowTerminatingError(new(new NotSupportedException("The ScriptBlock contains multiple statements or pipeline elements. "
+                                                                        + "Currently only a single external command is supported: raw { cmd }."),
+                                              "NotSupported",
+                                              ErrorCategory.NotImplemented,
+                                              this));
+                    return;
+                }
             }
             else if (Command is not null)
             {
@@ -191,23 +205,42 @@ public class InvokeRawCommandCommand : RawCommandBase
         }
     }
 
-    private (ApplicationInfo AppInfo, string[] Arguments) GetCommandAndArguments(ScriptBlock scriptBlock)
+    private static bool SniffScriptBlock(ScriptBlock scriptBlock, [MaybeNullWhen(false)] out CommandAst commandAst)
     {
+        commandAst = null;
         var ast = scriptBlock.Ast as ScriptBlockAst;
-        var cmdAst = ast?.EndBlock.Statements.OfType<PipelineAst>()
-                                             .SelectMany(stmt => stmt.PipelineElements)
-                                             .FirstOrDefault(cmdAst => cmdAst is CommandAst) as CommandAst
-                     ?? throw new InvalidOperationException("raw: no command specified");
+        if (ast is null)
+            return false;
 
-        var arguments = cmdAst.CommandElements.Skip(1)
-                                              .Select(elem => elem switch
-                                              {
-                                                  StringConstantExpressionAst str => str.Value,
-                                                  ExpandableStringExpressionAst exp => exp.Value,
-                                                  _ => elem.Extent.Text
-                                              });
-        return (GetAppInfo(cmdAst.GetCommandName()), arguments.ToArray());
+        if (ast.BeginBlock?.Statements.Count > 0)
+            return false;
+
+        if (ast.ProcessBlock?.Statements.Count > 0)
+            return false;
+
+        if (ast.EndBlock.Statements.Count != 1)
+            return false;
+
+        var pipeAst = ast.EndBlock.Statements[0] as PipelineAst;
+        if (pipeAst is null)
+            return false;
+
+        if (pipeAst.PipelineElements.Count != 1)
+            return false;
+
+        commandAst = pipeAst.PipelineElements[0] as CommandAst;
+
+        return commandAst is not null;
     }
+
+    private static IEnumerable<string> GetArguments(CommandAst commandAst)
+        => commandAst.CommandElements.Skip(1)
+                                     .Select(elem => elem switch
+                                      {
+                                          StringConstantExpressionAst str => str.Value,
+                                          ExpandableStringExpressionAst exp => exp.Value,
+                                          _ => elem.Extent.Text
+                                      });
 
     private ApplicationInfo GetAppInfo(string name)
             => InvokeCommand.GetCommand(name, CommandTypes.Application) as ApplicationInfo
