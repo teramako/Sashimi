2a2823d10666dce1d53b687786e676fbd7ecb903
teramako <teramako@gmail.com>
2026-07-11 20:49:21 +0900

    ♻️ Move `RawExecutionEngine` into Sashimi.Internal namespace

--- src/Internal/RawExecutionEngine.cs
+++ src/Internal/RawExecutionEngine.cs
@@ -5,9 +5,8 @@ using System.IO.Pipes;
 using System.Management.Automation;
 using System.Runtime.CompilerServices;
 using System.Text;
-using Sashimi.Internal;
 
-namespace Sashimi;
+namespace Sashimi.Internal;
 
 internal sealed class RawExecutionEngine : ExecutionEngine
 {
